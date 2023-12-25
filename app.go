533e640d1f34569800649f11cbd1329235831f7c
DEEPAK MALIK <97176010+deepakmalikundostres@users.noreply.github.com>
2023-12-25 23:18:46 +0530

    Add files via upload

---
+++ app.go
@@ -0,0 +1,10 @@
+package main // we can change this package but we need to prod we have to tell
+// where go to start entry point
+// while building the go
+// also remember there can we only one function inside a package
+
+func main() {
+	bank();
+	// profitCalculator()
+	// profitCalculatorV2()
+}
